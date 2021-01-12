81bb3b602c27a95bdefd3d0e402fdaad7252d468
dawidb1 <dawid.baranski.96@gmail.com>
2021-01-12 21:11:00 +0100

    add function

--- libs/node1/src/lib/Node1.spec.ts
+++ libs/node1/src/lib/Node1.spec.ts
@@ -1,7 +1,14 @@
-import { node1 } from './node1';
+import { node1, sum } from './node1';
 
 describe('node1', () => {
   it('should work', () => {
     expect(node1()).toEqual('node1');
+  });
+});
+
+
+describe('sum', () => {
+  it('should calculate', () => {
+    expect(sum(1, 2)).toEqual(3);
   });
 });
--- libs/node1/src/lib/Node1.ts
+++ libs/node1/src/lib/Node1.ts
@@ -1,3 +1,7 @@
 export function node1(): string {
   return 'node1';
 }
+
+export function sum(first: number, second: number): number {
+  return first + second;
+}
