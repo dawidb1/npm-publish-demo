daf7d4f2bb03ac67c0fd0a9fc890f78750ec22c1
dawidb1 <dawid.baranski.96@gmail.com>
2021-01-12 21:10:54 +0100

    init empty node1

---
+++ libs/node1/src/lib/Node1.spec.ts
@@ -0,0 +1,7 @@
+import { node1 } from './node1';
+
+describe('node1', () => {
+  it('should work', () => {
+    expect(node1()).toEqual('node1');
+  });
+});
---
+++ libs/node1/src/lib/Node1.ts
@@ -0,0 +1,3 @@
+export function node1(): string {
+  return 'node1';
+}
